1d5bde03b2fa2076fc81584c2488fb68568e9b93
Alejandra Patricia Figueroa Jarquin <ia.minitita@ufg.edu.sv>
2024-08-13 19:59:57 -0600

    Creación de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Alejandra Patricia Figueroa Jarquín</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+
+    <body>
+<html>   
